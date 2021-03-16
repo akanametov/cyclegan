0907c334a1d805d36edbbc740ae634d64adf88d0
Azamat Kanametov <akkanametov@gmail.com>
2021-03-16 22:06:20 +0300

    Delete utils.py

--- model/utils.py
+++
@@ -1,6 +0,0 @@
-import torch
-from torch import nn
-
-def initialize_weights(layer, mean=0., std=0.02):
-    if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.ConvTranspose2d):
-        torch.nn.init.normal_(layer.weight, mean, std)
